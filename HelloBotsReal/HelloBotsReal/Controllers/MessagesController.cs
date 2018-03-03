429e6b029e02570926b22faea5213a11606697d8
alexc Escritorio <alexci1996@hotmail.com>
2018-03-02 21:55:30 -0600

    Ejercicio 2

--- HelloBotsReal/HelloBotsReal/Controllers/MessagesController.cs
+++ HelloBotsReal/HelloBotsReal/Controllers/MessagesController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using Microsoft.Bot.Connector;
 
 namespace HelloBotsReal.Controllers
 {
+    [BotAuthentication]
     public class MessagesController : ApiController
     {
     }
---
+++ HelloBotsReal/HelloBotsReal/Dialogs/MyDialog.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Threading.Tasks;
+
+namespace HelloBotsReal.Dialogs
+{
+    [Serializable]
+    public class MyDialog:IDialog<string>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var ActivityMessage = await result as Activity;
+            var Message = ActivityMessage.Text;
+            await context.PostAsync($"Escribiste: {Message}");
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
