f2047c39c0b1ab8c3c46e89a307ec60f0d7ebfb0
alexc Escritorio <alexci1996@hotmail.com>
2018-03-03 22:16:51 -0600

    Ejercicio 4 Terminado

--- HelloBotsReal/HelloBotsReal/Controllers/MessagesController.cs
+++ HelloBotsReal/HelloBotsReal/Controllers/MessagesController.cs
@@ -5,11 +5,42 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Bot.Connector;
+using Microsoft.Bot.Builder.Dialogs;
+using System.Threading.Tasks;
 
 namespace HelloBotsReal.Controllers
 {
     [BotAuthentication]
     public class MessagesController : ApiController
     {
+        void HandleSystemMessage(Activity message)
+        {
+            switch(message.Type)
+            {
+                case ActivityTypes.ConversationUpdate:
+                    break;
+                case ActivityTypes.ContactRelationUpdate:
+                    break;
+                case ActivityTypes.Typing:
+                    break;
+                case ActivityTypes.Ping:
+                    break;
+                case ActivityTypes.DeleteUserData:
+                    break;
+            }
+        }
+        public async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        {
+            if(activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.MyDialog());
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+            var Response = Request.CreateResponse(HttpStatusCode.OK);
+            return Response;
+        }
     }
 }
